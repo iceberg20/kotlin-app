0e9c565cc29971632a9fc1cb5ce5ba9c4a1791f2
Mateus Oliveira <mateusocb@gmail.com>
2019-08-15 17:06:02 -0300

    Adicionando a leitura de NFC no cadastro de patrimônio

--- app/src/main/java/com/kotlin_maps/inserirPatrim.kt
+++ app/src/main/java/com/kotlin_maps/inserirPatrim.kt
@@ -16,16 +16,43 @@ import androidx.core.content.ContextCompat
 
 import kotlinx.android.synthetic.main.activity_inserir_patrim.*
 import kotlinx.android.synthetic.main.activity_maps.*
+import android.app.PendingIntent
+import android.graphics.Color
+import android.nfc.NfcAdapter
+import android.provider.Settings
+import android.widget.Toast
+import kotlin.experimental.and
+
 
 class inserirPatrim : AppCompatActivity(), View.OnClickListener {
 
     private val CAMERA_REQUEST_CODE = 12345
     private val REQUEST_GALLERY_CAMERA = 45654
 
+    var nfcAdapter: NfcAdapter? = null
+    var pendingIntent: PendingIntent? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inserir_patrim)
         capture.setOnClickListener(this)
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+
+        if (nfcAdapter == null) {
+            Toast.makeText(this, "Cadê teu NFC?", Toast.LENGTH_SHORT).show()
+
+            finish()
+
+            return
+        }
+
+        pendingIntent = PendingIntent.getActivity(
+            this, 0,
+            Intent(this, this.javaClass)
+                .addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0
+        )
+
     }
 
     override fun onClick(v: View?) {
@@ -68,4 +95,52 @@ class inserirPatrim : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    override fun onNewIntent(intent: Intent) {
+        setIntent(intent)
+        resolveIntent(intent)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (nfcAdapter != null) {
+            if (!nfcAdapter!!.isEnabled)
+                showWirelessSettings()
+
+            nfcAdapter?.enableForegroundDispatch(this, pendingIntent, null, null);
+        }
+    }
+
+    private fun showWirelessSettings() {
+        Toast.makeText(this, "You need to enable NFC", Toast.LENGTH_SHORT).show()
+        intent = Intent(Settings.ACTION_WIRELESS_SETTINGS)
+        startActivity(intent)
+    }
+
+    private fun resolveIntent(intent: Intent) {
+
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.action)
+            || NfcAdapter.ACTION_TECH_DISCOVERED.equals(intent.action)
+            || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.action)) {
+
+            val id: ByteArray = intent.getByteArrayExtra(NfcAdapter.EXTRA_ID)
+
+            tagPatri.setText(toDec(id).toString())
+            tagPatri.isEnabled = false
+            tagPatri.setTextColor(Color.BLUE)
+        }
+    }
+
+    private fun toDec(bytes: ByteArray): Long {
+        var result: Long = 0
+        var factor: Long = 1
+        for (i in bytes.indices) {
+
+            val value = bytes[i].and(0xffL.toByte())
+
+            result += value * factor
+            factor *= 256L
+        }
+        return result
+    }
 }
