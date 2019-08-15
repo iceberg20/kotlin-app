5b4c3543666a36354328ff3a81b49f29bb752340
Italo Berg Marques Souza dos Reis <italo.berg.residente@tre-rn.jus.br>
2019-08-15 18:27:59 -0300

    api parcial 1

--- app/src/main/java/com/kotlin_maps/MapsActivity.kt
+++ app/src/main/java/com/kotlin_maps/MapsActivity.kt
@@ -1,7 +1,11 @@
 package com.kotlin_maps
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,6 +13,10 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_maps.*
+import java.io.BufferedInputStream
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -21,6 +29,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        GetJsonWithOkHttpClient(this.text).execute()
     }
 
     /**
@@ -40,5 +50,51 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.addMarker(MarkerOptions().position(sydney).title("TRE-RN"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(-5.8105124, -35.2001164), 15.0f))
+    }
+
+    open class GetJsonWithOkHttpClient(textView: TextView) : AsyncTask<Unit, Unit, String>() {
+
+        val mInnerTextView = textView
+
+        override fun doInBackground(vararg params: Unit?): String? {
+            val networkClient = NetworkClient()
+            val stream = BufferedInputStream(
+                networkClient.get(urlN))
+            return readStream(stream)
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+
+            mInnerTextView.text = result
+
+        }
+
+        fun readStream(inputStream: BufferedInputStream): String {
+            val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+            val stringBuilder = StringBuilder()
+            bufferedReader.forEachLine { stringBuilder.append(it) }
+            return stringBuilder.toString()
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.action_settings) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    companion object {
+
+        @JvmField
+        // val url = "https://raw.githubusercontent.com/irontec/android-kotlin-samples/master/common-data/bilbao.json"
+        val urlN = "https://server2.planetgroupbd.com/ords/pepsi/v1/outlet/118786"
+
     }
 }
